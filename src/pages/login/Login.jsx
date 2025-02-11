f94f32fceaff6bd686fde81af85ed10360551e74
hhj8159 <hhj8159@gmail.com>
2025-02-11 10:33:47 +0900

    font custom

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,9 +2,16 @@ import React from "react";
 
 function Login(){
   return (
-    <div>
-      <h1>Login index</h1>
-    </div>
+    <form>
+      <div>
+        <h1>Login index</h1>
+      </div>      
+      <div>
+
+      </div>
+
+
+    </form>
   );
 }
 
